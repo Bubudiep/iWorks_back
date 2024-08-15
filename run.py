6f6729ee9822f49faae7d5926d973c807549164a
Bubudiep <hlitdvp98@gmail.com>
2024-08-15 23:43:14 +0700

    update key

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 # run.py
 from app import create_app
-
+import platform
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0', port=5000)
+    if platform.system() == "Windows":
+        app.run(ssl_context=('cert.pem', 'key.pem'),debug=True,host='0.0.0.0', port=5000)
+    else:
+        app.run(ssl_context='adhoc',debug=True,host='0.0.0.0', port=5000)
