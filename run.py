318500bcc271bd39a9021aa3b9adebcafe80f873
bubu_diep <hlitdvp98@gmail.com>
2024-08-16 09:12:08 +0700

    remove ssl

--- run.py
+++ run.py
@@ -5,6 +5,15 @@ app = create_app()
 
 if __name__ == '__main__':
     if platform.system() == "Windows":
-        app.run(ssl_context=('cert.pem', 'key.pem'),debug=True,host='0.0.0.0', port=5000)
+        app.run(
+            # ssl_context=('cert.pem', 'key.pem'),
+            debug=True,
+            host='0.0.0.0', 
+            port=5000
+        )
     else:
-        app.run(ssl_context='adhoc',debug=True,host='0.0.0.0', port=5000)
+        app.run(
+            # ssl_context='adhoc',
+            debug=True,host='0.0.0.0', 
+            port=5000
+        )
